14cfdb7133c6ef38afc95e5363b1d8c68701c7b6
Vishvnath Pratap Singh <33184243+Vishvnath96@users.noreply.github.com>
2017-11-18 19:39:39 +0530

    Use this file to import your target's public headers
    Use this file to import your target's public headers that you would like to expose to Swift

---
+++ UiTests-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#import "Memory.h"
+#import "MemoryResource.h"
+#import "NFRHelperKer.h"
+#import "CpuResource.h"
+#import "NetworkResource.h"
